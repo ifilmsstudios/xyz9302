0abcf6c7b2a273ab9cde8bb7354e47ba38b08419
iFilms Botz <92921270+ifilmsbotz@users.noreply.github.com>
2022-07-05 08:15:59 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -54,8 +54,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/arshsisodiya/helios-mirror")
-    buttons.buildbutton("Support Group", "https://t.me/mirrorsociety")
+    buttons.buildbutton("Channel", "https://t.me/iFilms_Studios")
+    buttons.buildbutton("Group", "https://t.me/iFilms_Support")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
